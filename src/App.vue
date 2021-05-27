3716d68f1826632261e3ae72cfc12fa019d64499
smn.singh444 <smn.singh444@gmail.com>
2021-05-26 22:23:11 -0300

    Form Validation Exam

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <signupform/>
+</template>
+
+<script>
+import signupform from './components/signupform.vue'
+
+export default {
+  name: 'App',
+  components: {      
+    signupform
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
